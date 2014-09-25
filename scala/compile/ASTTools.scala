4f81f73dbe728dc455db7f8af80e89d8138aed16
Miles Steele <miles@milessteele.com>
2014-09-25 03:11:05 -0400

    fix parsing types

--- scala/compile/ASTTools.scala
+++ scala/compile/ASTTools.scala
@@ -4,7 +4,7 @@ import org.antlr.runtime.tree.ParseTree
 
 // Tools for handling ASTs.
 object ASTTools {
-  class ASTConstructionException() extends RuntimeException
+  class ASTConstructionException(msg: String) extends RuntimeException(msg)
 
   class HappyParseTree(pt: ParseTree) {
     def text = pt.getText()
@@ -27,7 +27,7 @@ object ASTTools {
     pt.children(0).text match {
       case "callout_decl" => pt.children.map(parseCalloutDecl(_)).toList
       case "<epsilon>" => List()
-      case _ => throw new ASTConstructionException
+      case _ => throw new ASTConstructionException("wtf")
     }
 
   def parseCalloutDecl(pt: ParseTree): CalloutDecl =
@@ -37,36 +37,36 @@ object ASTTools {
     pt.children(0).text match {
       case "field_decl" => pt.children.map(parseFieldDecl(_)).toList
       case "<epsilon>" => List()
-      case _ => throw new ASTConstructionException
+      case _ => throw new ASTConstructionException("wtf")
     }
 
   def parseFieldDecl(pt: ParseTree): FieldDecl = {
     // TODO only handles simplest case fields.
-    val dtype = parseDType(pt.children(0).children(0).text)
+    val dtype = parseDType(pt.children(0))
     val id = pt.children(1).children(0).text
     FieldDecl(dtype, id, None)
   }
 
-  def parseDType(s: String): DType = s match {
+  def parseDType(pt: ParseTree): DType = pt.text match {
+    case "type" => parseDType(pt.children(0))
     case "int" => DTInt
     case "boolean" => DTBool
     case "void" => DTVoid
-    case _ => throw new ASTConstructionException
+    case _ => throw new ASTConstructionException("Unknown type " + pt.text)
   }
-
 
   def parseMethodDecls(pt: ParseTree): List[MethodDecl] =
     pt.children(0).text match {
       case "method_decl" => pt.children.map(parseMethodDecl(_)).toList
       case "<epsilon>" => List()
-      case _ => throw new ASTConstructionException
+      case _ => throw new ASTConstructionException("wtf")
     }
 
   def parseMethodDecl(pt: ParseTree): MethodDecl = {
     // TODO args and block are fake.
     val id = pt.children(1).text
     val args = List()
-    val returns = parseDType(pt.children(0).text)
+    val returns = parseDType(pt.children(0))
     val block = Block(List(), List())
     MethodDecl(id, args, returns, block)
   }
