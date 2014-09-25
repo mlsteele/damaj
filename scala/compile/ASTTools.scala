1de4ab36a79ec33cfb6114ffe28996c8742110ed
Miles Steele <miles@milessteele.com>
2014-09-25 03:02:56 -0400

    remove dead code

--- scala/compile/ASTTools.scala
+++ scala/compile/ASTTools.scala
@@ -18,12 +18,6 @@ object ASTTools {
   implicit def parseTreeUpgrade(pt: ParseTree): HappyParseTree =
     new HappyParseTree(pt)
 
-  // Get the children of a ParseTree node.
-  def pNodeChildren(tree: ParseTree): List[ParseTree] =
-    Range(0, tree.getChildCount())
-      .map(tree.getChild(_).asInstanceOf[ParseTree])
-      .toList
-
   def parseProgram(pt: ParseTree): ProgramAST = ProgramAST(
     parseCalloutDecls(pt.children(0)),
     parseFieldDecls(pt.children(1)),
@@ -75,33 +69,6 @@ object ASTTools {
     val returns = parseDType(pt.children(0).text)
     val block = Block(List(), List())
     MethodDecl(id, args, returns, block)
-  }
-
-  // Parse a ParseTree into an AST node.
-  // The AST type returned depends on the ParseTree input.
-  def parseNode(pt: ParseTree): Any = {
-    val kind = pt.getText()
-    val children = pNodeChildren(pt)
-    kind match {
-      case "program" => {
-        // TODO don't do it.
-        val callouts = List()
-        val fields = List(parseNode(children(0)).asInstanceOf[FieldDecl])
-        val methods = List()
-        ProgramAST(callouts, fields, methods)
-      }
-      case "field_decl" => {
-        val dtype: DType = parseNode(children(0)).asInstanceOf[DType]
-        // TODO only works for single simple decls.
-        val id = pNodeChildren(children(1))(0).getText()
-        FieldDecl(dtype, id, None)
-      }
-      case "type" => children(0).getText() match {
-        case "int" => DTInt
-        case "boolean" => DTBool
-      }
-      case _ => throw new ASTConstructionException
-    }
   }
 
 }
