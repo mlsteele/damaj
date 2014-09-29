c416f6648b71b90cda4ec331602dfa7b2c8644ed
Miles Steele <miles@milessteele.com>
2014-09-29 13:20:37 -0400

    fix indentation

--- scala/compile/ASTtoIR.scala
+++ scala/compile/ASTtoIR.scala
@@ -7,48 +7,51 @@ import SymbolTable._
 // variable names come from the naming convention 
 // established there
 //
-// to do: put callouts in symbol table
+// TODO: put callouts in symbol table
 //Construct an IR from a AST
 object IRBuilder{
   class IRConstructionException(msg: String) extends RuntimeException(msg)
 
   def convertProgram(ast: AST.ProgramAST): IR.ProgramIR = IR.ProgramIR( //what is the end of this line doing?
-    //to do
-   ast.callouts.map(convertCalloutDecl),
-   ast.fields.map(convertFieldDecl),
-   ast.methods.map(convertMethodDecl))
-   def convertCalloutDecl(calloutDec: AST.CalloutDecl): IR.Callout = IR.Callout(calloutDec.id)
-//     calloutList.map(AST.CalloutDecl => IR.CalloutDecl)
-   def convertFieldDecl(fieldDec: AST.FieldDecl): IR.Field = IR.Field(fieldDec.dtype,field.id,field.size)
-   def convertMethodDecl(meth: AST.MethodDecl): IR.Method = {
-     var method:IR.Method = IR.Method( meth.id,meth.args,meth.returns,meth.block,meth.params)
-     var table = new SymbolTable()
-     table.add(method)
-     method
-    }
+    // TODO
+    ast.callouts.map(convertCalloutDecl),
+    ast.fields.map(convertFieldDecl),
+    ast.methods.map(convertMethodDecl))
+    def convertCalloutDecl(calloutDec: AST.CalloutDecl): IR.Callout = IR.Callout(calloutDec.id)
+    // calloutList.map(AST.CalloutDecl => IR.CalloutDecl)
+    def convertFieldDecl(fieldDec: AST.FieldDecl): IR.Field = IR.Field(fieldDec.dtype,field.id,field.size)
+    def convertMethodDecl(meth: AST.MethodDecl): IR.Method = {
+    var method:IR.Method = IR.Method( meth.id,meth.args,meth.returns,meth.block,meth.params)
+    var table = new SymbolTable()
+    table.add(method)
+    method
+  }
      
   def convertAssignment(assign: AST.Assignment): IR.Assignment = IR.Assignment(locToStore(assign.left),convertExpr(assign.right))
 
   def convertExpr(expr: AST.Expr ): IR.Expr = IR.BoolLiteral(true)
-/* to do  def convertID()
-  def convertMethodDeclArg( )
-  def convertDType( )
- */
+
+  // TODO
+  // def convertID()
+  // def convertMethodDeclArg( )
+  // def convertDType( )
+
   def convertStatement(ast:AST.Statement): IR.Statement = ast match{
-      case a:AST.Assignment => convertAssignment(a)
-      case a:AST.MethodCall => convertMethodCall(a)
-      case a:AST.If => convertIf(a)
-      case a:AST.For => convertFor(a)
-      case a:AST.While => convertWhile(a)
-      case a:AST.Return => convertReturn(a)
-      case AST.Continue => IR.Continue
-     }
+    case a:AST.Assignment => convertAssignment(a)
+    case a:AST.MethodCall => convertMethodCall(a)
+    case a:AST.If => convertIf(a)
+    case a:AST.For => convertFor(a)
+    case a:AST.While => convertWhile(a)
+    case a:AST.Return => convertReturn(a)
+    case AST.Continue => IR.Continue
+  }
 
   def locToStore(loc: AST.Location): IR.Store= {
     val table = new SymbolTable
     val field:IR.Field = table.lookup(byID(loc.id))
-    //IR.Store(field,loc.index)
+    // IR.Store(field,loc.index)
   }
+
   def convertBlock(block: AST.Block ):IR.Block = IR.Block(block.decls,block.stmts)
   def convertIf(iff: AST.If): IR.If = IR.If(convertExpr(iff.condition),convertBlock(iff.then),iff.elseb.map(convertBlock))
   def convertFor(fo: AST.For): IR.For = IR.For(fo.id,convertExpr(fo.start),convertExpr(fo.iter),convertBlock(fo.then))
@@ -61,5 +64,4 @@ object IRBuilder{
   def convertStrLiteral(string: AST.StrLiteral): IR.StrLiteral = IR.StrLiteral(string.value)
   def convertCharLiteral(char: AST.CharLiteral): IR.IntLiteral = IR.IntLiteral(char.value.toInt)
   //  convertMany[AST.CalloutDecl](ast,"callout_many",convertCalloutDecl)
-
 }
