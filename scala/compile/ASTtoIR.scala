7cf44010afeeb014c565fed5f4deab8c3cfb29ca
Andres Perez <andres15perez@gmail.com>
2014-09-29 01:42:19 -0400

    ready to merge things fa real.

--- scala/compile/ASTtoIR.scala
+++ scala/compile/ASTtoIR.scala
@@ -20,43 +20,40 @@ object IRBuilder{
    def convertCalloutDecl(calloutDec: AST.CalloutDecl): IR.Callout = IR.Callout(calloutDec.id)
 //     calloutList.map(AST.CalloutDecl => IR.CalloutDecl)
    def convertFieldDecl(fieldDec: AST.FieldDecl): IR.Field = IR.Field(fieldDec.dtype,field.id,field.size)
-   def convertMethodDecl(meth: AST.MethodDecl): IR.Method {
-     val method = IR.Method( meth.id,meth.args,meth.returns,meth.block,meth.params)
-     symboltable.add(method)
+   def convertMethodDecl(meth: AST.MethodDecl): IR.Method = {
+     var method:IR.Method = IR.Method( meth.id,meth.args,meth.returns,meth.block,meth.params)
+     var table = new SymbolTable()
+     table.add(method)
      method
-     /*  def convertMethodDeclAgr(meth: AST.MethodDeclArg): IR.Load = meth match{
+    }
      
-   }*/
-   //for convert assign assume know how to convert location to store  
   def convertAssignment(assign: AST.Assignment): IR.Assignment = IR.Assignment(locToStore(assign.left),convertExpr(assign.right))
 
   def convertExpr(expr: AST.Expr ): IR.Expr = IR.BoolLiteral(true)
-  def convertID()
+/* to do  def convertID()
   def convertMethodDeclArg( )
   def convertDType( )
+ */
   def convertStatement(ast:AST.Statement): IR.Statement = ast match{
-      case Assignment => convertAssignment(ast)
-      case MethodCall => convertMethodCall(ast)
-      case If => convertIf(ast)
-      case For => convertFor(ast)
-      case While => convertWhile(ast)
-      case Return => convertReturn(ast)
-      case Continue => convertContinue(ast)
+      case a:AST.Assignment => convertAssignment(a)
+      case a:AST.MethodCall => convertMethodCall(a)
+      case a:AST.If => convertIf(a)
+      case a:AST.For => convertFor(a)
+      case a:AST.While => convertWhile(a)
+      case a:AST.Return => convertReturn(a)
+      case AST.Continue => IR.Continue
      }
-  
 
-  def locToStore(loc: AST.Location): IR.Store{
-    table = new SymbolTable
+  def locToStore(loc: AST.Location): IR.Store= {
+    val table = new SymbolTable
     val field:IR.Field = table.lookup(byID(loc.id))
-    return IR.Store(field,loc.index
-
-
+    //IR.Store(field,loc.index)
   }
   def convertBlock(block: AST.Block ):IR.Block = IR.Block(block.decls,block.stmts)
-  def convertIf(iff: AST.If): IR.If = IR.If(iff.condition,iff.then,iff.elseb)
-  def convertFor(fo: AST.For): IR.For = IR.For(fo.id,fo.start,fo.iter,fo.then)
-  def convertWhile (whil:AST.While): IR.While = IR.While(whil.condition, whil.block,whil.max)
-  def convertReturn (ret:AST.Return): IR.Return = IR.Return(ret.expr)
+  def convertIf(iff: AST.If): IR.If = IR.If(convertExpr(iff.condition),convertBlock(iff.then),iff.elseb.map(convertBlock))
+  def convertFor(fo: AST.For): IR.For = IR.For(fo.id,convertExpr(fo.start),convertExpr(fo.iter),convertBlock(fo.then))
+  def convertWhile (whil:AST.While): IR.While = IR.While(convertExpr(whil.condition),convertBlock(whil.block),whil.max)
+  def convertReturn (ret:AST.Return): IR.Return = IR.Return(ret.expr.map(convertExpr))
   //  def convertBreak ( ) this can be done through =>
   //  def convertContinue ( ) this can be done through =>
   def convertIntLiteral(int:AST.IntLiteral): IR.IntLiteral = IR.IntLiteral(int.value)
