a06ff8845b1b3d6fcf77ee6d08eb0de7f6af22f6
Diony Rosa <dhrosa@gmail.com>
2014-10-09 19:09:48 -0400

    Added skeletal CFGBuilder file.

---
+++ scala/compile/IRToCFG.scala
@@ -0,0 +1,13 @@
+package compile
+
+import IR._
+import IRShared._
+
+// Construct an AST from a parse tree
+// ptree - root program node
+// source - source code
+// Example Usage:
+//   val ast = ASTBuilder.parseProgram(parseTree).ast
+class CFGBuilder(program: ProgramIR, filepath: String, code: String) {
+
+}
