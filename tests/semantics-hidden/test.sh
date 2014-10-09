d2709f2ef4a6237287b7c427f3b2021bbd2c3a2d
Miles Steele <miles@milessteele.com>
2014-10-09 02:22:13 -0400

    fix test script

--- tests/semantics-hidden/test.sh
+++ tests/semantics-hidden/test.sh
@@ -5,19 +5,20 @@ runsemantics() {
 }
 
 fail=0
+here=`dirname $0`
 
-for file in `dirname $0`/illegal/*; do
+for file in `find $here/illegal/ -type f`; do
   if runsemantics $file > /dev/null 2>&1; then
     echo "Illegal file $file semantic checked successfully.";
     fail=1
   fi
 done
 
-for file in `dirname $0`/legal/*; do
+for file in `find $here/legal/ -type f`; do
   if ! runsemantics $file; then
     echo "Legal file $file failed to pass semantic checks.";
     fail=1
   fi
 done
 
-exit $fail;
+exit $fail
