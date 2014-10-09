9e711ef8d43797cf34733237e22fd89dd46bf15a
Miles Steele <miles@milessteele.com>
2014-10-09 19:49:29 -0400

    add compile script

---
+++ compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+runcompiler() {
+    $(git rev-parse --show-toplevel)/run.sh --opt=all --target=assembly -o $2 $1
+}
+
+compile() {
+    gcc -o $2 $1
+}
+
+if [[ -z "$1" ]]; then
+    echo "run with an input decaf program to compile"
+    exit
+fi
+
+root=$(git rev-parse --show-toplevel)
+asm="$root/tmp/out.S"
+binary="$root/tmp/out"
+mkdir -p "$root/tmp"
+
+runcompiler "$1" $asm &&\
+echo "$asm" &&\
+compile $asm $binary &&\
+echo "$binary"
