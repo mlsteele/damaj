25b75946397d42160a1ec82bb9baf8f159bcdc35
Jess Kenney <seveneightn9ne@gmail.com>
2014-10-10 17:05:27 -0400

    redo CFG

--- scala/compile/CFG.scala
+++ scala/compile/CFG.scala
@@ -4,17 +4,22 @@ object CFG {
   import SymbolTable._
   import IR._
   import IRShared._
+  
+  // Earlier version constrain all fields to come before methods.
+  // The callouts are discarded.
+  case class Program(fields: List[Field], methods: List[Method])
 
-  case class Program(fields: List[FieldSymbol], methods: List[MethodSymbol])
+  case class Field(id: ID, size: Option[Long])
+  case class Method(id: ID, params: List[Field], start: Block, edges: IdentityMap[Block, Transition])
   
   // Not the same as an IR block! Cannot contain any control flow statements.
   // The lecture notes from 10/9 explain the idea.
-  case class Block(stmts: List[Statement], symbols: SymbolTable, next: Transition)
+  case class Block(stmts: List[Statement])
   type Statement = IR.Statement
 
   sealed trait Transition
   case class Edge(to: Block) extends Transition
   case class Fork(condition: IR.Expr, ifTrue: Block, ifFalse: Block) extends Transition
-  case class End() extends Transition
+  case class End(from: Block) extends Transition
 
 }
