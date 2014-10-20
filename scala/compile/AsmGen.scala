b9da72e18d7ddea8c66c106c60cbb671f0798a2c
Andres Perez <andres15perez@gmail.com>
2014-10-19 22:37:33 -0400

    made pushall popall macros

--- scala/compile/AsmGen.scala
+++ scala/compile/AsmGen.scala
@@ -439,7 +439,47 @@ object AsmDSL {
   def comment(a: String): String = s"# $a"
   def arrayAccess(displacement: Int, baseReg: String, offsetReg: String, multiplier: Int) =
     s"$displacement($baseReg, $offsetReg, $multiplier)"
-
+  // We push all registers in following orde:
+  // (rax, rbx, rcx, rdx, rbp, rsp, rsi, rdi,
+  // r8, r9, r10, r11, r12, r13, r14, r15) 
+  // and pop in reverse order
+  def pushall(a: String): String ={
+    push(rax)
+    push(rbx)
+    push(rcx)
+    push(rdx)
+    push(rbp)
+    push(rsp)
+    push(rsi)
+    push(rdi)
+    push(r8)
+    push(r9)
+    push(r10)
+    push(r11)
+    push(r12)
+    push(r13)
+    push(r14)
+    push(r15)
+  }
+  
+  def popall(a: String): String ={
+    pop(r15)
+    pop(r14)
+    pop(r13)
+    pop(r12)
+    pop(r11)
+    pop(r10)
+    pop(r9)
+    pop(r8)
+    pop(rdi)
+    pop(rsi)
+    pop(rsp)
+    pop(rbp)
+    pop(rdx)
+    pop(rcx)
+    pop(rbx)
+    pop(rax)
+  }
   // stackvars is the number of vars in the method's stack frame
   def method(label: String, stackvars: Int, body: String): String = {
     // TODO(miles): push and pop all regs.
