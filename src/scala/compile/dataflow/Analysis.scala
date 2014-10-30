aa4c61592b7e722c195e3b5d61aa8191435927da
Diony Rosa <dhrosa@gmail.com>
2014-10-30 00:12:49 -0400

    Added members to Analysis class.

--- src/scala/compile/dataflow/Analysis.scala
+++ src/scala/compile/dataflow/Analysis.scala
@@ -1,8 +1,49 @@
+package compile
+
+/**
+  * Abstract class reprenting a dataflow analysis.  To implement an
+  * analysis, you must define the following methods:
+  * - bottom
+  * - direction
+  * - merge
+  * - transfer
+  */
 abstract trait Analysis {
-  type T;
+  type T; // The datatype of the internal state, usually a set
 
   /*
-   * The merging operator for sets from each path.
+   * The member representing nothing, a default state.  For any
+   * set-based Analysis, this is most likely going to be the empty
+   * set.
    */
-  def confluenceOperator(a:Set[T], b: Set[T]) : Set[T]
+  def bottom() : T
+
+  /**
+    * The direction of the analysis must be specified as either
+    * forward or backwards.
+    */
+  abstract sealed trait AnalysisDirection;
+  case class Forward()  extends AnalysisDirection;
+  case class Backward() extends AnalysisDirection;
+  def direction() : AnalysisDirection
+
+  /*
+   * The merging operator for analysis results from each path
+   */
+  def merge(a:T, b:T) : T
+
+  /*
+   * The transfer function. This takes as input the previous state,
+   * the current statement, and returns a new state.
+   */
+  def transfer(previous: T, stmt: IR2.Statement) : T
+
+  /*
+   * Runs the dataflow analysis using the overriden
+   * operations. Returns the information known about each statement.
+   */
+  final def analyze(method: IR2.Method) : Map[IR2.Statement, T] = {
+    // TODO: actually run the dataflow algorithm
+    return Map()
+  }
 }
