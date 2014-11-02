734208990da310f0ca592268358fcf1216de7a5c
Diony Rosa <dhrosa@gmail.com>
2014-11-02 16:18:13 -0500

    Refactored redundant analysis code

--- src/scala/compile/dataflow/AvailableExpressions.scala
+++ src/scala/compile/dataflow/AvailableExpressions.scala
@@ -2,7 +2,7 @@ package compile
 
 object AvailableExpressions extends Analysis {
   import IR2._
-  import IRShared.StrLiteral
+  import ExprDependencies._
 
   type T = Set[Expr]
 
@@ -31,22 +31,5 @@ object AvailableExpressions extends Analysis {
     return avail
   }
 
-  private implicit class BetterExpr(e: Expr) {
-    // Set of loads that this expression depends on
-    def dependencies(): Set[Load] = e match {
-      case Call(_, args) => {
-        var deps : Set[Load] = Set()
-        args.foreach {
-          case Left(_) =>
-          case Right(subE) => deps = deps union subE.dependencies()
-        }
-        deps
-      }
-      case BinOp(left, _, right) => Set(left, right)
-      case UnaryOp(_, right) => Set(right)
-      case Ternary(cond, left, right) => Set(cond, left, right)
-      case LoadField(_, index) => index.map(_.dependencies()).getOrElse(Set())
-      case _:LoadLiteral => Set()
-    }
-  }
+
 }
--- src/scala/compile/dataflow/LiveVariables.scala
+++ src/scala/compile/dataflow/LiveVariables.scala
@@ -8,6 +8,8 @@ package compile
   */
 object LiveVariables extends Analysis {
   import IR2._
+  import ExprDependencies._
+
   type T = Set[Load]
 
   def bottom() = Set()
@@ -35,25 +37,4 @@ object LiveVariables extends Analysis {
     }
     return live
   }
-
-  //TODO: make this code not copy-pasta
-  private implicit class BetterExpr(e: Expr) {
-    // Set of loads that this expression depends on
-    def dependencies(): Set[Load] = e match {
-      case Call(_, args) => {
-        var deps : Set[Load] = Set()
-        args.foreach {
-          case Left(_) =>
-          case Right(subE) => deps = deps union subE.dependencies()
-        }
-        deps
-      }
-      case BinOp(left, _, right) => Set(left, right)
-      case UnaryOp(_, right) => Set(right)
-      case Ternary(cond, left, right) => Set(cond, left, right)
-      case LoadField(_, index) => index.map(_.dependencies()).getOrElse(Set())
-      case _:LoadLiteral => Set()
-    }
-  }
-
 }
