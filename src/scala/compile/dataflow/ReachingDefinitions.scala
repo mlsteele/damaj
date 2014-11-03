5a82a111098011bde1b529a87788cefc3ab17e68
Diony Rosa <dhrosa@gmail.com>
2014-11-03 10:51:44 -0500

    Added ReachingDefinitions analysis

---
+++ src/scala/compile/dataflow/ReachingDefinitions.scala
@@ -0,0 +1,32 @@
+package compile
+
+/*
+ * After each block, this analysis returns the set of assignments that
+ * reach (were not overwritten) after that block's execution.
+ */
+object ReachingDefinitions extends Analysis {
+  import IR2._
+
+  type T = Set[Assignment]
+
+  def bottom() = Set()
+
+  def direction() = Forward
+
+  def merge(a:T, b:T) = a union b
+
+  def transfer(previous: T, block: Block) : T = {
+    var reaching: Set[Assignment] = previous
+    for (stmt <- block.stmts) stmt match {
+      case ass@Assignment(_,_) => {
+        // Kill any assignments with the same store
+        reaching = reaching filter { s => s.left != ass.left}
+        // Add this assignment to the reaching defs
+        reaching = reaching + ass
+      }
+      case _:Call => // don't care
+      case _:Return => // don't care
+    }
+    return reaching
+  }
+}
