46542ffe64cbcdf811e405e87fbd9830609e023d
Andres <andres15perez@gmail.com>
2014-11-04 20:51:07 -0500

    made a graphing script

---
+++ graph.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if command -v add >/dev/null 2>&1; 
+then
+    add -f scala
+fi
+gitroot=$(git rev-parse --show-toplevel)
+scala -classpath $gitroot/build/:$gitroot/vendor/antlr.jar compile.Compiler "$@"
+dot -Tps tmp/graph.gv -o tmp/graph.ps
+
