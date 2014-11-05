cad286f4354d3e1e7aa437a80f181f7b58e0d3e6
Miles Steele <miles@milessteele.com>
2014-11-04 23:46:13 -0500

    force debug in graph.sh

--- graph.sh
+++ graph.sh
@@ -4,6 +4,6 @@ then
     add -f scala
 fi
 gitroot=$(git rev-parse --show-toplevel)
-scala -classpath $gitroot/build/:$gitroot/vendor/antlr.jar compile.Compiler "$@"
+scala -classpath $gitroot/build/:$gitroot/vendor/antlr.jar compile.Compiler -d "$@"
 dot -Tsvg tmp/graph.gv -o tmp/graph.svg
 dot -Tpng tmp/graph.gv -o tmp/graph.png
