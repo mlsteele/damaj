1140435bd67cc69003387149fa6bd6988fe80a04
Miles Steele <miles@milessteele.com>
2014-11-05 12:26:00 -0500

    tweak graph.sh

--- graph.sh
+++ graph.sh
@@ -4,9 +4,16 @@ then
     add -f scala
 fi
 gitroot=$(git rev-parse --show-toplevel)
+
+# remove old graphs
+rm -vf tmp/*.gv
+rm -vf tmp/*.gv.*
+
+# generate .gv's
 scala -classpath $gitroot/build/:$gitroot/vendor/antlr.jar compile.Compiler -d -o /dev/null "$@"
-rm -v tmp/*.gv.*
+
+# generate .svg's
 for file in `dirname $0`/tmp/*.gv; do
-  echo "rendering graph: $file to .svg, .png"
+  echo "rendering graph: $file.svg"
   dot -Tsvg $file -o $file.svg
 done
