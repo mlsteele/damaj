066c610a70c48cc4ac9317b49e356a32e39c2e2f
Diony Rosa <dhrosa@gmail.com>
2014-11-05 22:52:35 -0500

    Added Reachable code analysis

---
+++ src/scala/dataflow/Reachable.scala
@@ -0,0 +1,30 @@
+package compile
+
+/**
+  * Analyzes which statements are reachable.  The outputs represent
+  * whether statements after a given block are reachable
+  */
+class Reachable(override val cfg: CFG) extends Analysis {
+  import IR2._
+
+  type T = Boolean
+
+  def initial() = true
+
+  def bottom() = false
+
+  def direction() = Forward
+
+  def merge(a: T, b: T) : T = a || b
+
+  def transfer(previous: T, block: Block) : T = {
+    // If the parents are unreachable, then any statement afterward is unreachable
+    if (previous == false) return false
+    // If this block has a return, then any blocks afterwards are unreachable
+    val hasReturn: Boolean = block.stmts.exists {
+      case _:Return => true
+      case _ => false
+    }
+    return !hasReturn
+  }
+}
