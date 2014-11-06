046d774932fa050b1b1741bf721122f76cbf7aa9
Miles Steele <miles@milessteele.com>
2014-11-05 20:06:09 -0500

    loads depend on themselves

--- src/scala/Util.scala
+++ src/scala/Util.scala
@@ -75,22 +75,21 @@ class IdentityMap[K,V] {
   * Utilities for analyzing what variables a expression depends on.
   */
 object ExprDependencies {
+  import IRShared._
   import IR2._
 
   implicit class BetterExpr(e: Expr) {
     // Set of loads that this expression depends on
     def dependencies(): Set[Load] = e match {
-      case Call(_, args) => {
-        var deps : Set[Load] = Set()
-        args.foreach {
-          case Left(_) =>
-          case Right(subE) => deps = deps union subE.dependencies()
-        }
-        deps
-      }
+      case Call(_, args) =>
+        args.toSet.flatMap{ x: Either[StrLiteral, Expr] => x match {
+          case Left(str) => List()
+          case Right(subE) => subE.dependencies().toList
+        }}
       case BinOp(left, _, right) => Set(left, right)
       case UnaryOp(_, right) => Set(right)
-      case LoadField(_, index) => index.map(_.dependencies()).getOrElse(Set())
+      case l@LoadField(_, index) =>
+        Set(l) ++ index.map(_.dependencies()).getOrElse(Set())
       case _:LoadLiteral => Set()
     }
   }
