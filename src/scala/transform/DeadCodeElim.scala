81f6ef88cb84caa07aca4c4cb2dff083921dce0b
Diony Rosa <dhrosa@gmail.com>
2014-11-05 21:35:09 -0500

    Mostly implemented dead code elimination

--- src/scala/transform/DeadCodeElim.scala
+++ src/scala/transform/DeadCodeElim.scala
@@ -15,18 +15,40 @@ object DeadCodeElim {
 
   def transformMethod(method: Method) : Method = {
     val results = (new LiveVariables(method.cfg)).analyze()
-    val inputs = results.inputs
-    val outputs = results.outputs
+    val liveAfter = results.inputs
+    val liveBefore = results.outputs
 
+    // Pretty print analysis results
     def annotate(b: Block) : String = {
-      val inputsTitle = "=========\nINPUTS\n=========\n"
-      val inputsString = inputs(b).mkString("\n") + "\n"
-      val outputsTitle = "=========\nOUTPUTS\n=========\n"
-      val outputsString = outputs(b).mkString("\n")
+      val inputsTitle = "=========\nLIVE BEFORE\n=========\n"
+      val inputsString = liveBefore(b).mkString("\n") + "\n"
+      val outputsTitle = "=========\nLIVE AFTER\n=========\n"
+      val outputsString = liveAfter(b).mkString("\n")
       return inputsTitle + inputsString + outputsTitle + outputsString
     }
 
     Grapher.graph(method, "deadcode.live", Some(annotate(_)))
-    return method
+
+    val newCFG = method.cfg.mapBlocks { b =>
+      b.stmts.filter{
+        case Assignment(Store(to, index), _) => {
+          val variable = LoadField(to, index)
+          // If this variable is not live after this block, there is no point in keeping this assignment
+          liveAfter(b) contains variable
+        }
+        case c:Call => true
+        case r:Return => true
+      }
+    }
+
+    val newLocals = method.locals
+    return Method(method.id,
+      method.params,
+      newLocals,
+      newCFG,
+      method.returnType
+    )
   }
+
+  private implicit def stmtsToBlock(stmts: List[Statement]) : Block = Block(stmts)
 }
