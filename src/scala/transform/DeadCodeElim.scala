dd3b0a5fc9b0a9570a4f54d248ad55edd10a8e47
Diony Rosa <dhrosa@gmail.com>
2014-11-05 20:37:27 -0500

    Made DeadCodeElim use LiveVariables instead of ReachingDefinitions

--- src/scala/transform/DeadCodeElim.scala
+++ src/scala/transform/DeadCodeElim.scala
@@ -15,18 +15,17 @@ object DeadCodeElim {
 
   def transformMethod(method: Method) : Method = {
     print("TRANSFORMING %s\n".format(method.id))
-    val results = (new ReachingDefinitions(method.cfg)).analyze()
+    val results = (new LiveVariables(method.cfg)).analyze()
     //val inputs = results.inputs
     val outputs = results.outputs
 
     def annotate(b: Block) : String = {
       val title = "%s outputs\n".format(b.uuid)
-      var contents = ""
-      for (ass <- outputs(b)) {
-        contents += ass.toString + "\n"
-      }
+      val contents = outputs(b).toList.map(v => "%s\n".format(v)).mkString
       return title + contents
     }
+
+    print(results)
 
     Grapher.graph(method, "deadcode", Some(annotate(_)))
     return method
