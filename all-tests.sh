82cfe67892d03179576a9170c2db648cfd666c6f
Miles Steele <miles@milessteele.com>
2014-11-11 17:42:39 -0500

    add all-tests.sh

---
+++ all-tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+#
+# Script to run all tests
+# Does NOT build first
+#
+
+echo "all-tests: skipping scanner tests"
+# tests/scanner/test.sh
+# tests/scanner-hidden/test.sh
+
+echo "all-tests: running parser tests"
+tests/parser/test.sh
+tests/parser-hidden/test.sh
+
+echo "all-tests: running semantics tests"
+tests/semantics/test.sh
+tests/semantics-hidden/test.sh
+
+echo "all-tests: running codegen tests"
+tests/codegen/test.sh
+tests/codegen-hidden/test.sh
+
+echo "all-tests: running dataflow tests"
+tests/dataflow/test.sh
