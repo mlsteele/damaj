fcd06b730ffbf81824a18ef216fe3c130fc06e46
Jess Kenney <seveneightn9ne@gmail.com>
2014-11-15 15:36:14 -0500

    zero work done on register allocation

---
+++ src/scala/RegisterAllocation.scala
@@ -0,0 +1,13 @@
+package compile
+
+object RegisterAllocation {
+  /*import IR2._
+  
+  def findConstraints(program: Program): Something = {
+    val results = (new LiveVariables(method)).analyze()
+    val liveAfter = results.inputs
+
+    
+  }*/
+  
+}
